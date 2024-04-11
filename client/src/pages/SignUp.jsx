60bc1edd20b3331739f7412a461300a4c2259815
anusha7530 <agarwalanusha13@gmail.com>
2024-04-11 14:51:19 +0530

    Complete sign-in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -83,6 +83,7 @@ export default function SignUp() {
                 placeholder="Password"
                 id="password"
                 onChange={handleChange}
+                autoComplete="off"
               />
             </div>
             <Button
